91c230f358b27b16b1e9fedbf80a4b011e477e24
Brandon Freitag <freitagbr@gmail.com>
2017-11-18 13:26:05 -0800

    Rename idents

--- set1/c1.c
+++ set1/c1.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
-static const unsigned char b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const unsigned char eb64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-void base64decode(const unsigned char *src, unsigned char *dst, int len) {
-    dst[0] =             b64[(int) (  src[0]         >> 2)                            ];
-    dst[1] =             b64[(int) (((src[0] & 0x03) << 4) | (((src[1] & 0xF0) >> 4)))];
-    dst[2] = (len > 1) ? b64[(int) (((src[1] & 0x0F) << 2) | (((src[2] & 0xC0) >> 6)))] : '=';
-    dst[3] = (len > 2) ? b64[(int) (  src[2] & 0x3F)                                  ] : '=';
+void base64encode(const unsigned char *src, unsigned char *dst, int len) {
+    dst[0] =             eb64[(int) (  src[0]         >> 2)                            ];
+    dst[1] =             eb64[(int) (((src[0] & 0x03) << 4) | (((src[1] & 0xF0) >> 4)))];
+    dst[2] = (len > 1) ? eb64[(int) (((src[1] & 0x0F) << 2) | (((src[2] & 0xC0) >> 6)))] : '=';
+    dst[3] = (len > 2) ? eb64[(int) (  src[2] & 0x3F)                                  ] : '=';
 }
 
 void error(const char *msg) {
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
     unsigned char *dst = (unsigned char *) malloc(sizeof (unsigned char) * dstlen);
 
     for (int s = 0, d = 0; s < srclen; s += 3, d += 4) {
-        base64decode(&src[s], &dst[d], srclen - s);
+        base64encode(&src[s], &dst[d], srclen - s);
     }
 
     printf("%s\n", dst);
