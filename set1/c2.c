5ac0d7d3641f4eda80bb28c88a4c695d4a0df281
Brandon Freitag <freitagbr@gmail.com>
2017-11-17 23:44:18 -0800

    Challenge 2

---
+++ set1/c2.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void error(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        error("two arguments required");
+    }
+
+    const unsigned char *a_inp = (unsigned char *) argv[1];
+    const unsigned char *b_inp = (unsigned char *) argv[2];
+    const int alen = strlen((const char *) a_inp);
+    const int blen = strlen((const char *) b_inp);
+
+    if (alen != blen) {
+        error("inputs must the same length");
+    }
+
+    if ((alen % 2) != 0) {
+        error("inputs must be valid hex strings");
+    }
+
+    const int len = (alen + (alen % 2)) / 2;
+    const unsigned char *a = (unsigned char *) malloc(sizeof (unsigned char) * len);
+    const unsigned char *b = (unsigned char *) malloc(sizeof (unsigned char) * len);
+
+    for (int i = 0, s = 0; i < alen; i += 2, s += 1) {
+        int ra = sscanf((const char *) &a_inp[i], "%2hhx", (unsigned char *) &a[s]);
+        int rb = sscanf((const char *) &b_inp[i], "%2hhx", (unsigned char *) &b[s]);
+        if ((ra != 1) || (rb != 1)) {
+            error("inputs must be valid hex strings");
+        }
+    }
+
+    printf("%s\n", a);
+    printf("%s\n", b);
+
+    unsigned char *c = (unsigned char *) malloc(sizeof (unsigned char) * len);
+
+    for (int i = 0, s = 0; i < len; i += 1, s += 2) {
+        sprintf((char *) &c[s], "%02x", a[i] ^ b[i]);
+    }
+
+    printf("%s\n", c);
+    exit(EXIT_SUCCESS);
+}
