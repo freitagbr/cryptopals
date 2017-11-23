063fba3acc184f707fbbce7bcba89da3804a5882
Brandon Freitag <freitagbr@gmail.com>
2017-11-22 20:52:29 -0800

    Add -Wextra to compiler flags

--- inc.mk
+++ inc.mk
@@ -1,6 +1,6 @@
 CC       = cc
 CXX      = c++
-CFLAGS   = -Wall -O
-CXXFLAGS = -Wall -O --std=c++11
+CFLAGS   = -Wall -Wextra -O
+CXXFLAGS = -Wall -Wextra -O --std=c++11
 LDFLAGS  = -O
 LDLIBS   = -lm
