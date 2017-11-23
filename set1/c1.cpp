32f6e53b4461d2e6e403898055b7ddb6b3dda162
Brandon Freitag <freitagbr@gmail.com>
2017-11-22 17:51:21 -0800

    Refactor c2 in c++

--- set1/c1.cpp
+++ set1/c1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "base64.h"
+#include "hex.h"
 #include "../error.h"
 
 int main(int argc, char **argv) {
@@ -10,17 +11,8 @@ int main(int argc, char **argv) {
         return error("one argument required");
     }
 
-    std::string hex(argv[1]);
-    std::vector<uint8_t> bytes;
-    size_t i = 0;
-
-    while (i < hex.length()) {
-        const char *chars = hex.substr(i, 2).c_str();
-        const uint8_t byte = (uint8_t) strtol(chars, nullptr, 16);
-        bytes.push_back(byte);
-        i += 2;
-    }
-
+    const std::string src(argv[1]);
+    const std::vector<uint8_t> bytes = hex::parse(src);
     std::string dst;
 
     if (!base64::encode(bytes, &dst)) {
