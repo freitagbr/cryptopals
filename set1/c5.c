850c482958867cd50d44498bba2887761cbd38b5
Brandon Freitag <freitagbr@gmail.com>
2017-11-25 15:01:47 -0800

    Revert "Refactor c5 in c++"
    This reverts commit 11f25fda1dd0fad15a7976ae95f8d28da5148cb3.

---
+++ set1/c5.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static const unsigned char key[] = "ICE";
+
+int error(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    return EXIT_FAILURE;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        error("one argument required");
+    }
+
+    const unsigned char *src = (const unsigned char *) argv[1];
+    const int len = strlen((const char *) src);
+    const unsigned char *dst = (unsigned char *) malloc(sizeof (unsigned char) * len * 2);
+
+    for (int i = 0, s = 0, k = 0; i < len; i += 1, s += 2, k = (k + 1) % 3) {
+        sprintf((char *) &dst[s], "%02x", src[i] ^ key[k]);
+    }
+
+    printf("%s\n", dst);
+    free((void *) dst);
+
+    return EXIT_SUCCESS;
+}
